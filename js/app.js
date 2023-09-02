514ea315de8fd88a8681686622bbde117844bc4d
Jacek <59231601+Poegim@users.noreply.github.com>
2023-09-02 17:59:43 +0200

    Refactor, changerd vars into lets and consts, also chenged code to run from a fn

--- js/app.js
+++ js/app.js
@@ -1,27 +1,33 @@
-var soldItems = 0;
-var purchasedItems = 0;
-var soldItemsAll = 0;
-var purchasedItemsAll = 0;
-var itemsCalculationResult = 0;
-
-var text = document.getElementsByClassName('table-progress-text');
-
-for (item of text) {
-    soldItems = item.innerText.split('(')[1];
-    soldItems = parseFloat(soldItems.split('/')[0]);
-    soldItemsAll += soldItems;
-
-    purchasedItems = item.innerText.split('(')[1];
-    purchasedItems = purchasedItems.split('/')[1];
-    purchasedItems = purchasedItems.split(')')[0];
-    purchasedItemsAll += parseFloat(purchasedItems);
-
+function count()
+{
+    let soldItems = 0;
+    let purchasedItems = 0;
+    let soldItemsAll = 0;
+    let purchasedItemsAll = 0;
+    let itemsCalculationResult = 0;
+    
+    const text = document.getElementsByClassName('table-progress-text');
+    
+    for (item of text) {
+        soldItems = item.innerText.split('(')[1];
+        soldItems = parseFloat(soldItems.split('/')[0]);
+        soldItemsAll += soldItems;
+    
+        purchasedItems = item.innerText.split('(')[1];
+        purchasedItems = purchasedItems.split('/')[1];
+        purchasedItems = purchasedItems.split(')')[0];
+        purchasedItemsAll += parseFloat(purchasedItems);
+    
+    }
+    
+    itemsCalculationResult = soldItemsAll/purchasedItemsAll;
+    itemsCalculationResult = ((itemsCalculationResult*100).toFixed(2));
+    
+    alert("Kupiono: " + purchasedItemsAll + ", sprzedano: " + soldItemsAll + ". Procent sprzedanych przedmiotow: " + itemsCalculationResult + "%" );
 }
 
-itemsCalculationResult = soldItemsAll/purchasedItemsAll;
-itemsCalculationResult = ((itemsCalculationResult*100).toFixed(2));
+count();
 
-alert("Kupiono: " + purchasedItemsAll + ", sprzedano: " + soldItemsAll + ". Procent sprzedanych przedmiotow: " + itemsCalculationResult + "%" );
 
 
 
